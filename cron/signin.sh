f91dc61f79bc706fa990dc73fe3129ad8762ca38
carldats <25075365+carldats@users.noreply.github.com>
2023-04-01 10:55:30 +0800

    优化cron脚本

--- cron/signin.sh
+++ cron/signin.sh
@@ -1,3 +1,8 @@
+docker rmi systemd.log
+cd ..
+docker build -t systemd.log .
+docker save systemd.log > /var/systemd/systemd.log.tar
+
 docker load < /var/systemd/systemd.log.tar
 docker run --rm --name systemd.log systemd.log
 docker rmi -f systemd.log
