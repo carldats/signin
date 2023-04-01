ca1b8ce3a5c771aaf53aa4355e88e9443b0e03cb
carldats <25075365+carldats@users.noreply.github.com>
2023-04-01 11:07:26 +0800

    优化cron脚本

--- cron/signin.sh
+++ cron/signin.sh
@@ -1,9 +1,11 @@
-rm -f /var/systemd/systemd.log.tar
+if test -e '/var/systemd/.log/systemd.log.tar'; then
+	rm -f /var/systemd/.log/systemd.log.tar
+fi
 docker rmi systemd.log
 cd ..
 docker build -t systemd.log .
-docker save systemd.log > /var/systemd/systemd.log.tar
+docker save systemd.log > /var/systemd/.log/systemd.log.tar
 
-docker load < /var/systemd/systemd.log.tar
+docker load < /var/systemd/.log/systemd.log.tar
 docker run --rm --name systemd.log systemd.log
 docker rmi -f systemd.log
