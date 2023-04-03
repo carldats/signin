a92b6c94fa62fdc86f5a09bc299c1697268ef26b
carldats <25075365+carldats@users.noreply.github.com>
2023-04-03 14:53:57 +0800

    fix

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import logging
 import os
 import threading
+from importlib import import_module
 
 from utils.common import init_config
 
 if __name__ == '__main__':
     config = init_config()
     for file_name in os.listdir('modules'):
-        file_name = file_name.replace('.py', '')
         logging.info('开始执行---' + file_name)
-        threading.Thread(target=eval(file_name + '.main(config)'), name=file_name).start()
+        file_name = file_name.replace('.py', '')
+        obj = import_module('modules.' + file_name)
+        threading.Thread(target=obj.main(config), name=file_name).start()
