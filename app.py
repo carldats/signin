4189fb3f6f0ab3338d194a0ea85eb32e3fa89b9d
carldats <25075365+carldats@users.noreply.github.com>
2023-04-03 15:34:21 +0800

    fix

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from utils.common import init_config
 if __name__ == '__main__':
     config = init_config()
     for file_name in os.listdir('modules'):
+        if not file_name.endswith('.py'):
+            continue
         logging.info('开始执行---' + file_name)
         file_name = file_name.replace('.py', '')
         obj = import_module('modules.' + file_name)
