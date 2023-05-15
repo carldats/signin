bdbb33c7162e34f9e42af7bec51cffc9ad205c0a
carldats <25075365+carldats@users.noreply.github.com>
2023-05-15 12:17:48 +0800

    优化

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-# import logging
-# import os
-# from importlib import import_module
-from modules import aliyundrive, kutogroup, lixianla
+import logging
+import os
+from importlib import import_module
+
 from utils.common import init_config
 
 if __name__ == '__main__':
     config = init_config()
-    # for file_name in os.listdir('modules'):
-    #     if not file_name.endswith('.py'):
-    #         continue
-    #     logging.info('#################################### ' + file_name + ' ####################################')
-    #     file_name = file_name.replace('.py', '')
-    #     import_module('modules.' + file_name).run(config)
-    aliyundrive.run(config)
-    kutogroup.run(config)
-    lixianla.run(config)
+    modules = os.listdir('modules')
+    modules.sort()
+    for file_name in modules:
+        try:
+            if not file_name.endswith('.py'):
+                continue
+            logging.info('#################################### ' + file_name + ' ####################################')
+            file_name = file_name.replace('.py', '')
+            import_module('modules.' + file_name).run(config)
+        except Exception as e:
+            logging.error(e)
